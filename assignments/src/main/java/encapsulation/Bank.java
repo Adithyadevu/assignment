dad0855cf2695db8b17c47a0dcac72368908b5d3
acer <acer@DEVU>
2024-05-07 21:37:14 +0530

    Encapsulation

---
+++ assignments/src/main/java/encapsulation/Bank.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class Bank {
+private int pin;
+	
+	public void setPin(int pin) {
+		this.pin=pin;
+	}
+	public void getPin() {
+		System.out.println(pin);
+	}
+}
---
+++ assignments/src/main/java/encapsulation/User.java
@@ -0,0 +1,16 @@
+package encapsulation;
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bank obj=new Bank();
+		obj.setPin(6677);
+		obj.getPin();
+		obj.setPin(4006);
+		obj.getPin();
+		obj.setPin(2244);
+		obj.getPin();
+	}
+
+}
